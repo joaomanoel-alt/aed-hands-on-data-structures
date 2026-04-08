93e16d20c7bfff407cb6a1099fe38e8dbc10027c
joaomanoel-alt <joao.manoel@upe.br>
2026-04-08 11:24:12 -0300

    Add binary search implementation
    Implement binary search algorithm in the function.

--- src/binary_search.py
+++ src/binary_search.py
@@ -7,4 +7,16 @@ def binary_search(array: MyArray, target: int) -> int:
 
     Deve retornar o índice do elemento ou -1 caso não encontrado.
     """
-    raise NotImplementedError
+    esquerda = 0
+    direita = len(array) -1
+    
+    while esquerda <= direita:
+        mid = (esquerda + direita) // 2
+        
+        if array[mid] == target: 
+            return mid
+        elif array[mid] < target:
+            esquerda = mid + 1
+        else:
+            direita = mid - 1
+    return -1
